cbac719203a1eb0ec319875eeab19763a2bee273
Speleize <alexandre.suchot@ynov.com>
2019-04-01 08:51:35 +0200

    button onclick

--- app/src/main/java/com/speleize/alexl/viabrico/LoginActivity.java
+++ app/src/main/java/com/speleize/alexl/viabrico/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.speleize.alexl.viabrico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,5 +16,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToLogged(View view){
+        
+        Intent intent = new Intent(this, LoggedActivity.class);
+        startActivity(intent);
     }
 }
