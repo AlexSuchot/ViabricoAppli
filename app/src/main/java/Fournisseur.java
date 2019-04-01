37cd821c03c610b99a59ff98940b75212eca5887
Speleize <alexandre.suchot@ynov.com>
2019-04-01 08:33:27 +0200

    Structure of project done

--- app/src/main/java/Fournisseur.java
+++ app/src/main/java/Fournisseur.java
@@ -1,2 +1,55 @@
 public class Fournisseur {
+    private String Name;
+    private String Description;
+    private String Address;
+    private Integer Number;
+    private String Email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public Integer getNumber() {
+        return Number;
+    }
+
+    public void setNumber(Integer number) {
+        Number = number;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Fournisseur(String name, String description, String address, Integer number, String email) {
+        Name = name;
+        Description = description;
+        Address = address;
+        Number = number;
+        Email = email;
+    }
 }
--- app/src/main/java/com/speleize/alexl/viabrico/LoginActivity.java
+++ app/src/main/java/com/speleize/alexl/viabrico/LoginActivity.java
@@ -2,8 +2,13 @@ package com.speleize.alexl.viabrico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
